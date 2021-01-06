3e0abab485a0ff68a4d195c47586adf38e452cec
tom131313 <tom131313@users.noreply.github.com>
2021-01-06 09:36:07 -0500

    Delete imageEdit.bat

--- imageEdit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem usage:imageEdit "E:\Pictures\Fair 7-20-09_39.jpg" "E:\Pictures\Vicki's camera\vicki camera 067.jpg"
-start "" "C:\Program Files\IrfanView\i_view64.exe" %1
-start "" /WAIT "C:\Program Files\IrfanView\i_view64.exe" %2
